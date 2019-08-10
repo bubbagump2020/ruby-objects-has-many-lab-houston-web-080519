6f5b9b788eff0d77d700a870fdea006e768492c5
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-10 18:24:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,16 +4,19 @@ class Artist
   def initialize(name)
    @name = name
    @songs = []
+   @song_count = 0
   end
   
   def add_song(song)
     @songs << song
     song.artist = self
+    @song_count += 1
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
+    @song_count += 1
   end
   
   def songs
@@ -22,7 +25,7 @@ class Artist
   
   
   def self.song_count
-    
+    return @song_count
   end
   
 end
